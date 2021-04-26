4dc4da91b9e84c3b7253760aad41d61589a682dd
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-04-26 19:49:13 +0600

    static

--- Qtec/settings.py
+++ Qtec/settings.py
@@ -51,13 +51,13 @@ AUTH_USER_MODEL = "courier.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Qtec.urls'
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'courier/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'Qtec/static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
